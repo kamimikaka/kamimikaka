c5f93386c8aea75c61c9a62eae89edc6dac29f32
kamimikaka <jessruss99@gmail.com>
2025-03-18 17:03:34 -0300

    Update kamimikaka.js

--- kamimikaka.js
+++ kamimikaka.js
@@ -1,4 +1,4 @@
 alert('Deu certo kkkkk');
-var testee = document.querySelector("#user-top > span.ml-10");
+var testee = document.querySelector("#user-top > a.ml-10");
 testee.innerText = 'KKKK eu hackeei você!';
 //kkkkkk
